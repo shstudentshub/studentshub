f9eacb055963a7b530e8359e4a22f576ceb18c35
kelvinclin <kelvinclin4u@gmail.com>
2018-01-03 12:23:24 +0000

    First addition

---
+++ StudentHub/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+    <link rel="stylesheet" href="css/style.css">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  </head>
+  <body>
+    <div class="navbar-fixed">
+      <nav class="navbar-change">
+          <div class="nav-wrapper">
+              <a href="#" class="logo">Logo</a> &nbsp
+              <ul class="right hide-on-med-and-down">
+                <li><a href="#">Sign in</a></li>
+                <li><a href="#" class="btn">Sign up</a></li>
+                <li><a href="#">Cart</a></li>
+              </ul>
+
+              <ul id="nav-mobile" class="side-nav">
+                <li><a href="#">Sign in</a></li>
+                <li><a href="#" class="btn">Sign up</a></li>
+                <li><a href="#">Cart</a></li>
+                <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+              </ul>
+          </div>
+      </nav>
+    </div>
+
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+  </body>
+</html>
