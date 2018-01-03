88853332151f17fe0ad8ff676976d42283b3fdd8
kelvinclin <kelvinclin4u@gmail.com>
2018-01-03 14:04:12 +0000

    Continuation

--- StudentHub/index.php
+++ StudentHub/index.php
@@ -13,25 +13,25 @@
     <div class="navbar-fixed">
       <nav class="navbar-change">
           <div class="nav-wrapper">
-              <a href="#" class="logo">Logo</a> &nbsp
-              <ul class="right hide-on-med-and-down">
+              <a href="#" class="logo hide-on-med-and-down">Logo</a> &nbsp
+              <ul class="right">
                 <li><a href="#">Sign in</a></li>
                 <li><a href="#" class="btn">Sign up</a></li>
                 <li><a href="#"><i class="tiny material-icons">add_shopping_cart</i></a></li>
               </ul>
-
-              <ul id="nav-mobile" class="side-nav">
-                <li><a href="#">Sign in</a></li>
-                <li><a href="#" class="btn">Sign up</a></li>
-                <li><a href="#">Cart</a></li>
-                <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+              <ul class="side-nav" id="nav-mobile">
+                <li><a href="#">Some links</a></li>
+                <li><a href="#">Some links</a></li>
+                <li><a href="#">Some links</a></li>
               </ul>
+              <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
           </div>
       </nav>
     </div>
 
       <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/init.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
   </body>
 </html>
