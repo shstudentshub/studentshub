9dcc0da49d4cb86a2d1e49a895c212e56c9f449b
kelvinclin <kelvinclin4u@gmail.com>
2018-01-03 21:05:29 +0000

    Some link contains menu

--- StudentHub/index.php
+++ StudentHub/index.php
@@ -5,8 +5,3 @@
 
 <!-- include the footer of the document -->
 <?php include "includes/footer.inc.php"; ?>
-
-<h1 class="center-align">The Main Body Content Comes Here. <br/> <i class="fa fa-smile-o"></i> Happy Coding Mr John.</h1>
-
-<!-- include the footer of the document -->
-<?php include "includes/footer.inc.php"; ?>
