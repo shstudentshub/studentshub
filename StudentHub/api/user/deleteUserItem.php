2eb5a2dcc77072b981701059e58a2b2a05f40c4d
Enoch Sowah <enoch.sowah16@gmail.com>
2018-01-16 06:57:52 -0800

    delete item api updated

--- StudentHub/api/user/deleteUserItem.php
+++ StudentHub/api/user/deleteUserItem.php
@@ -1,2 +1,26 @@
 <?php
-	
+	<?php
+	include "../db-config.php";
+	$response = array();
+
+	if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+		$itemId = intval($_POST['itemId']);
+
+		$deleteQuery = "DELETE FROM items WHERE item_id = ?";
+		$preparedQuery = $database->prepare($deleteQuery);
+		$preparedQuery->bind_param('i',$itemId);
+		
+		if ($preparedQuery->execute()) {
+			$response["success"] = true;
+			$response["message"] = "Item Deleted Successfully.";
+			header('Content-Type: application/json');
+		    echo json_encode($response);
+		} else {
+			$response["success"] = false;
+			$response["message"] = "Could Not Delete Item.";
+			header('Content-Type: application/json');
+		    echo json_encode($response);
+		}
+
+	}
