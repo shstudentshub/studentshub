705c37b5793e6f7ce81b5a218ff331f76a379269
Enoch Sowah <enoch.sowah16@gmail.com>
2018-01-23 20:58:48 -0800

    user dashboard update

--- api/user/getUserProfile.php
+++ api/user/getUserProfile.php
@@ -1,2 +1,20 @@
-<?php
-	echo "hello user";
+<?php 
+	include "../db-config.php";
+	session_start();
+
+	$userId = intval($_SESSION["userId"]);
+
+	$getQuery = "SELECT * FROM users WHERE user_id = $userId";
+
+	$result = $database->query($getQuery);
+
+	$row = $result->fetch_assoc();
+
+	$userName = $row["user_name"];
+	$userContact = $row["user_contact"];
+	$userEmail = $row["user_email"];
+
+	echo "
+		
+
+	";
